10162d3ace59ff1e2e74c5672198d972591badbc
Dan Danilyuk and Rebecca McIntosh <dan@>
2017-08-23 14:15:41 -0700

    add winner alert, and reset the page

--- js/scripts.js
+++ js/scripts.js
@@ -13,40 +13,73 @@ PigDice.prototype.diceToCurrent = function() {
     return this.currentScore += this.diceRoll;
   }
 };
-
+function hide1(diceRoll) {
+  if (diceRoll === 1) {
+    $("form#player1Form").hide();
+    $("form#player2Form").show();
+  }
+}
+function hide2(diceRoll) {
+  if (diceRoll === 1) {
+    $("form#player2Form").hide();
+    $("form#player1Form").show();
+  }
+}
+function winner(pushedScore1,pushedScore2) {
+  if (pushedScore1 >= 100) {
+    alert("Player1 Wins!")
+    pushedScore1 = 0;
+    location.reload();
+  } else if (pushedScore2 >= 100) {
+    alert("Player2 Wins!")
+    location.reload();
+  }
+}
 //Front End
 $(document).ready(function() {
 //Input Form 1
-var currentScore = 0;
-var totalScore = 0;
-var pushedScore = 0;
+var currentScore1 = 0;
+var currentScore2 = 0;
+var totalScore1 = 0;
+var totalScore2 = 0;
+var pushedScore1 = 0;
+var pushedScore2 = 0;
   $("form#player1Form").submit(function(event) {
     event.preventDefault();
     var diceRoll = Math.floor(Math.random() * 6) + 1
-    score = new PigDice(diceRoll,currentScore);
-    currentScore = score.diceToCurrent()
+    score = new PigDice(diceRoll,currentScore1);
+    currentScore1 = score.diceToCurrent()
     // alert(currentScore)
+    $("form#player1Form").show();
     $("#outputrole").text(diceRoll);
-    $("#outputscore").text(currentScore);
+    $("#outputscore").text(currentScore1);
+    hide1(diceRoll);
    });
     $("#hold").click(function() {
-    pushedScore = currentScore + pushedScore;
-    $("#outputscore").text(currentScore = 0);
-    $("#outputtotal").text(pushedScore);
+    pushedScore1 = currentScore1 + pushedScore1;
+    $("#outputscore").text(currentScore1 = 0);
+    $("#outputtotal").text(pushedScore1);
+    $("form#player1Form").hide();
+    $("form#player2Form").show();
+    winner(pushedScore1);
   });
   //Player2
   $("form#player2Form").submit(function(event) {
     event.preventDefault();
     var diceRoll = Math.floor(Math.random() * 6) + 1
-    score = new PigDice(diceRoll,currentScore);
-    currentScore = score.diceToCurrent()
+    score = new PigDice(diceRoll,currentScore2);
+    currentScore2 = score.diceToCurrent()
     // alert(currentScore)
     $("#outputrole2").text(diceRoll);
-    $("#outputscore2").text(currentScore);
+    hide2(diceRoll);
+    $("#outputscore2").text(currentScore2);
    });
     $("#hold2").click(function() {
-    pushedScore = currentScore + pushedScore;
-    $("#outputscore2").text(currentScore = 0);
-    $("#outputtotal2").text(pushedScore);
+    pushedScore2 = currentScore2 + pushedScore2;
+    $("#outputscore2").text(currentScore2 = 0);
+    $("#outputtotal2").text(pushedScore2);
+    $("form#player2Form").hide();
+    $("form#player1Form").show();
+    winner(pushedScore2);
   });
 });
